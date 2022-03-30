4713fda90f21b4fd7ae12b9e2c6dcfa1971c9a07
lencx <cxin1314@gmail.com>
2022-03-30 09:54:40 +0800

    chore: reset

--- scripts/reset.js
+++ scripts/reset.js
@@ -33,6 +33,7 @@ function cnameReset() {
 function pkgReset() {
   pkg.name = 'gg';
   pkg.description = 'A gatsby website builder based on github discussions';
+  pkg.scripts.posts = 'yarn post:base';
 
   fs.writeFile('package.json', JSON.stringify(pkg, null, 2), function (err) {
     if (err) return;
